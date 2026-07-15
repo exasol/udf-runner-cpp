e3b2b15fa9bbf6edf84121621274d32db49b3ab0
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2026-07-15 18:42:00 +0530

    target base_test_build_run

--- test_container/tests/test/smoke/smoke_test.py
+++ test_container/tests/test/smoke/smoke_test.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
-
 from exasol_python_test_framework import udf
 
 
@@ -9,13 +7,6 @@ class SmokeTest(udf.TestCase):
     def test_db_connection(self):
         rows = self.query("SELECT 1 FROM dual")
         self.assertRowsEqual([(1,)], rows)
-
-    def test_release_payload_present(self):
-        bucket_root = Path("/buckets/bfsdefault/myudfs")
-        language_definitions = list(bucket_root.glob("**/language_definitions.json"))
-        udf_clients = list(bucket_root.glob("**/exaudf/exaudfclient"))
-        self.assertGreaterEqual(len(language_definitions), 1)
-        self.assertGreaterEqual(len(udf_clients), 1)
 
 
 if __name__ == "__main__":
