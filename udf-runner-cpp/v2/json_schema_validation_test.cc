c7b68c71b4fe623f19bd8ef18eb4a5af7f755fec
Torsten Kilias <torsten.kilias@exasol.com>
2026-09-10 17:40:34 +0200

    Add schema loader diagnostics

--- udf-runner-cpp/v2/json_schema_validation_test.cc
+++ udf-runner-cpp/v2/json_schema_validation_test.cc
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <cstdlib>
 #include <fstream>
+#include <iostream>
 #include <stdexcept>
 #include <string>
 
@@ -21,8 +22,25 @@ isolated_nlohmann::json read_json(const std::string& path) {
 int main() {
     const auto import_schema = read_json("json_schema/import_specification.schema.json");
     isolated_nlohmann::json_schema::json_validator validator(
-        [](const isolated_nlohmann::json_uri&, isolated_nlohmann::json& schema) {
+        [](const isolated_nlohmann::json_uri& uri, isolated_nlohmann::json& schema) {
+            std::cerr << "schema loader request: url=" << uri.url()
+                      << ", location=" << uri.location()
+                      << ", path=" << uri.path()
+                      << ", fragment=" << uri.fragment() << '\n';
+
             schema = read_json("json_schema/connection_information.schema.json");
+
+            std::cerr << "schema loader response: source=json_schema/connection_information.schema.json"
+                      << ", type=" << schema.type_name() << ", keys=[";
+            bool first = true;
+            for (const auto& item : schema.items()) {
+                if (!first) {
+                    std::cerr << ',';
+                }
+                std::cerr << item.key();
+                first = false;
+            }
+            std::cerr << "]\n";
         });
     validator.set_root_schema(import_schema);
 
