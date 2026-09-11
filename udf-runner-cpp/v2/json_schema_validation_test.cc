e272597a1349c0959bc6333d6288bcdde1658f28
Torsten Kilias <torsten.kilias@exasol.com>
2026-09-11 11:13:41 +0200

    Inline column schemas and test supported types

--- udf-runner-cpp/v2/json_schema_validation_test.cc
+++ udf-runner-cpp/v2/json_schema_validation_test.cc
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 #include <exasol/udf/v2/json_schema.hpp>
 
@@ -20,6 +21,30 @@ isolated_nlohmann::json read_json(const std::string& path) {
 }  // namespace
 
 int main() {
+    const std::vector<std::string> column_types = {
+        "DOUBLE PRECISION", "DECIMAL", "DATE", "TIMESTAMP",
+        "TIMESTAMP WITH LOCAL TIME ZONE", "CHAR", "VARCHAR", "BOOLEAN",
+        "HASHTYPE", "GEOMETRY", "INTERVAL YEAR TO MONTH",
+        "INTERVAL DAY TO SECOND",
+    };
+    const auto make_column = [](const std::string& type) {
+        return isolated_nlohmann::json{
+            {"name", "COLUMN_" + type},
+            {"type", type},
+            {"type_name", type},
+        };
+    };
+
+    const auto column_metadata_schema = read_json("json_schema/column_metadata.schema.json");
+    isolated_nlohmann::json_schema::json_validator column_metadata_validator;
+    column_metadata_validator.set_root_schema(column_metadata_schema);
+    for (const auto& type : column_types) {
+        column_metadata_validator.validate({
+            {"input_columns", {make_column(type)}},
+            {"output_columns", isolated_nlohmann::json::array()},
+        });
+    }
+
     const auto import_schema = read_json("json_schema/import_specification.schema.json");
     const auto load_schema = [](const isolated_nlohmann::json_uri& uri,
                                 isolated_nlohmann::json& schema) {
@@ -30,8 +55,7 @@ int main() {
 
             const auto path = uri.path();
             const auto filename = path.substr(path.find_last_of('/') + 1);
-            if (filename != "connection_information.schema.json" &&
-                filename != "column.schema.json") {
+            if (filename != "connection_information.schema.json") {
                 throw std::runtime_error("unsupported schema reference: " + uri.url());
             }
             const auto source = "json_schema/" + filename;
@@ -48,10 +72,17 @@ int main() {
                 first = false;
             }
             std::cerr << "]\n";
-        };
+    };
 
     isolated_nlohmann::json_schema::json_validator validator(load_schema);
     validator.set_root_schema(import_schema);
+
+    for (const auto& type : column_types) {
+        validator.validate({
+            {"is_subselect", true},
+            {"subselect_column_specification", {make_column(type)}},
+        });
+    }
 
     const isolated_nlohmann::json valid = {
         {"is_subselect", true},
